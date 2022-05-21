fed939689e7dfa3db4228959645ea71f7d180340
Juri Micheli <juri.micheli@studenti.unitn.it>
2022-05-21 14:33:55 +0200

    multiple markers with popup

--- src/Components/infoPopup.tsx
+++ src/Components/infoPopup.tsx
@@ -1,45 +1,16 @@
-import { Marker, Popup } from "react-leaflet"
+import { Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import {useCallback, useMemo, useRef, useState} from "preact/compat"
 
-const defaultPosition = {
-  lat: 51.505,
-  lng: -0.09,
-  zoom: 13
-};
-
-export function DraggableMarker() {
-  const [draggable, setDraggable] = useState(false)
-  const [position, setPosition] = useState(defaultPosition)
-  const markerRef = useRef(null)
-  const eventHandlers = useMemo(
-    () => ({
-      dragend() {
-        const marker = markerRef.current
-        if (marker != null) {
-          setPosition(marker.getLatLng())
-        }
-      },
-    }),
-    [],
-  )
-  const toggleDraggable = useCallback(() => {
-    setDraggable((d) => !d)
-  }, [])
-
+export function InfoPopup(props) {
   return (
-    <Marker
-      draggable={draggable}
-      eventHandlers={eventHandlers}
-      position={position}
-      ref={markerRef}>
-      <Popup minWidth={90}>
-        <span onClick={toggleDraggable}>
-          {draggable
-            ? 'Marker is draggable'
-            : 'Click here to make marker draggable'}
-        </span>
-      </Popup>
-    </Marker>
-  )
+    <Popup minWidth={90}>
+      <ul>
+        <li>Type: {props.type}</li>
+        <li>Material: {props.material}</li>
+        <li>Size: {props.size}</li>
+        <li>Accessibility: {props.accessibility}</li>
+        <li>Flow: {props.flow}</li>
+      </ul>
+    </Popup>
+  );
 }
--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,73 @@
-import { MapContainer, TileLayer } from "react-leaflet"
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
-import { DraggableMarker } from "./Components/infoPopup";
+import { InfoPopup } from "./Components/infoPopup";
 
 const defaultPosition = {
   lat: 51.505,
   lng: -0.09,
-  zoom: 13
+  zoom: 13,
 };
 
-
-
-
+const boardsCoordinates = [
+  {
+    lat: 51.505,
+    lng: -0.09,
+    type: "board1",
+    size: "small",
+    accessibility: "3",
+    flow: "5",
+    material: "metal",
+  },
+  {
+    lat: 52.505,
+    lng: -1.09,
+    type: "board2",
+    size: "big",
+    accessibility: "5",
+    flow: "1",
+    material: "wood",
+  },
+  {
+    lat: 49.505,
+    lng: 2.09,
+    type: "board3",
+    size: "medium",
+    accessibility: "1",
+    flow: "2",
+    material: "wood",
+  },
+  {
+    lat: 37.505,
+    lng: 5.09,
+    type: "board4",
+    size: "giant",
+    accessibility: "2",
+    flow: "3",
+    material: "glass",
+  },
+];
 
 export function App() {
   const position: [number, number] = [defaultPosition.lat, defaultPosition.lng];
 
   return (
-    <MapContainer
-      className="map"
-      center={position}
-      zoom={defaultPosition.zoom}
-    >
-      <TileLayer  
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    <MapContainer className="map" center={position} zoom={defaultPosition.zoom}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <DraggableMarker />
+      {boardsCoordinates.map((board) => (
+        <Marker position={[board.lat, board.lng]}>
+          <InfoPopup
+            type={board.type}
+            material={board.material}
+            size={board.size}
+            accessibility={board.accessibility}
+            flow={board.flow}
+          />
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
-
