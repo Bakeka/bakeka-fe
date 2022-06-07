0e0e6555bc885a4d5597b2265968149419b48978
Juri Micheli <juri.micheli@studenti.unitn.it>
2022-06-07 19:35:33 +0200

    filters showing

--- src/components/filterBoards.tsx
+++ src/components/filterBoards.tsx
@@ -1,36 +1,70 @@
 import { Checkbox, useCheckboxState } from "pretty-checkbox-react";
 import { Accessibility, Material, Size, Type } from "../entities";
 
-const entities = [
-  Object.keys(Material).map((material) => {
-    return { name: material };
-  }),
-
-  Object.keys(Accessibility).map((accessibility) => {
-    return { name: accessibility };
-  }),
-
-  Object.keys(Size).map((size) => {
-    return { name: size };
-  }),
-
-  Array.from({ length: 5 }, (_, i: number) => i + 1).map((traffic) => {
-    return { name: traffic.toString() };
-  }),
-];
-
 export function FilterBoard() {
-  const types = Object.keys(Type).map((type) => {
-    return {
-      name: type,
-      value: useCheckboxState(),
-    };
-  });
+  const strings: string[] = [
+    "Type",
+    "Material",
+    "Accessibility",
+    "Size",
+    "Traffic",
+  ];
+
+  const entities = [
+    Object.keys(Type).map((type) => {
+      return {
+        name: type,
+        value: useCheckboxState(),
+      };
+    }),
+
+    Object.keys(Material).map((material) => {
+      return {
+        name: material,
+        value: useCheckboxState(),
+      };
+    }),
+
+    Object.keys(Accessibility).map((accessibility) => {
+      return {
+        name: accessibility,
+        value: useCheckboxState(),
+      };
+    }),
+
+    Object.keys(Size).map((size) => {
+      return {
+        name: size,
+        value: useCheckboxState(),
+      };
+    }),
+
+    Array.from({ length: 5 }, (_, i: number) => i + 1).map((traffic) => {
+      return {
+        name: traffic.toString(),
+        value: useCheckboxState(),
+      };
+    }),
+  ];
 
   return (
     <>
-      {types.map((entity) => {
-        return <Checkbox {...entity.value}>{entity.name}</Checkbox>;
+      {entities.map((entity, i) => {
+        return (
+          <div className="flex inline-flex flex-row bg-indigo-300 m-2 rounded">
+            <b className="mx-1 my-1">{strings[i]}:</b>
+            {entity.map((data) => {
+              return (
+                <Checkbox
+                  {...data.value}
+                  className="flex inline-flex flex-row mx-2 my-1"
+                >
+                  <span className="ml-1">{data.name}</span>
+                </Checkbox>
+              );
+            })}
+          </div>
+        );
       })}
     </>
   );
